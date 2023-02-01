8d027520b30147fa0505a3e005e36b40f4d01066
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-31 21:40:45 -0800

    completed part 3, added Switch to my Routes in App component

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import harvardArt from "./data/harvardArt.js"
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
@@ -6,10 +6,19 @@ import GalleryView from "./components/GalleryView";
 function App() {
   return (
     <div className="page-wrapper">
-    <GalleryNavigation galleries={harvardArt.records}/>
-    <Route exact path="/galleries/:galleryId">
-      <GalleryView gallerries={harvardArt.records} />
-    </Route>
+      <Switch>
+        <Route exact path="/">
+          <h2>Harvard Art Museum</h2>
+          <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+          <GalleryNavigation galleries={harvardArt.records}/>
+        </Route>
+        <Route exact path="/galleries/:galleryId">
+          <GalleryView gallerries={harvardArt.records} />
+        </Route>
+        <Route>
+          <h2>Page Not Found</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
