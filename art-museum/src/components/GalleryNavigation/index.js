1a0c204cb2c7f0d5232861d1d811df1e45de7960
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-31 16:03:35 -0800

    completed phase 1, completed GalleryNavigation component and linked to main application

--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,7 +1,29 @@
-function GalleryNavigation(){
+import { NavLink } from 'react-router-dom'
+import './GalleryNavigation.css'
+
+function GalleryNavigation({ galleries }){
+
+    console.log('Galleries: ', galleries)
+
     return (
+        <nav>
         <h1>Galleries</h1>
-    )
+                <NavLink to="/">
+                    Home
+                </NavLink>
+                <ul>
+                {
+                    galleries.map(gallery => (
+                        <NavLink to={`/galleries/${gallery.id}`}>
+                            <li key={gallery.id}>
+                            {gallery.name}
+                            </li>
+                        </NavLink>
+                    ))
+                }
+                </ul>
+        </nav>
+        )
 }
 
 
